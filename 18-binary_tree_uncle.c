4986b89bfa3e68ebf9fa28e9261da94fe0d6d3a7
rwwwn <ralbaraiki@gmail.com>
2025-04-12 01:16:17 +0000

    Style: Fix Betty warnings in binary_tree_uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,11 +11,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 if (!node || !node->parent || !node->parent->parent)
 return (NULL);
 
-if (!node || !node->parent || !node->parent->parent)
-    return (NULL);
-
 if (node->parent->parent->left == node->parent)
-    return (node->parent->parent->right);
+return (node->parent->parent->right);
 else
-    return (node->parent->parent->left);
+return (node->parent->parent->left);
 }
