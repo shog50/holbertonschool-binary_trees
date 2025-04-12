609cf014409c9e1f9dc6b198417b208bd8633a7d
rwwwn <ralbaraiki@gmail.com>
2025-04-12 01:12:31 +0000

    Fix: Remove dependency on binary_tree_sibling in binary_tree_uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,5 +11,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 if (!node || !node->parent || !node->parent->parent)
 return (NULL);
 
-return (binary_tree_sibling(node->parent));
+if (!node || !node->parent || !node->parent->parent)
+    return (NULL);
+
+if (node->parent->parent->left == node->parent)
+    return (node->parent->parent->right);
+else
+    return (node->parent->parent->left);
 }
